a809e2f9d19301f37fcb988c284ac7181fa9f464
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-05-05 22:53:02 +0530

    🆕feat:[HOTELS LIST] get hotel list from restaurant table.

---
+++ server/routes/getHostelList.js
@@ -0,0 +1,23 @@
+import express from "express";
+import connectToDatabase from "../db/postgresClient.js";
+const app = express();
+
+const getHostelList = async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const query = "SELECT * FROM restaurant";
+
+    // Execute the query
+    const result = await db.query(query);
+
+    console.log("FETCHED HOSTEL LIST FROM DB");
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to fetch restaurant data." });
+  }
+};
+
+export default getHostelList;
