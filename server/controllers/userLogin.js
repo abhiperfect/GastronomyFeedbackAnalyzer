da0703e3bdbd2f1236b8a4caa98ae428af75473a
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-05-06 17:33:26 +0530

    ⚒️chore: to handle user login separetly.

--- server/controllers/userLogin.js
+++ server/controllers/userLogin.js
@@ -1,5 +1,4 @@
 import bcrypt from 'bcrypt';
-import getOTPByEmail from "./getOTPByEmail.js";
 import jwt from 'jsonwebtoken';
 import connectToDatabase from "../db/postgresClient.js";
 
@@ -18,15 +17,14 @@ const userLogin = async (req, res) => {
 
     // Check if a row was found
     if (result.rows.length > 0) {
-      const hashedPassword = result.rows[0].password_hash;
+      const { password_hash, ...userData } = result.rows[0];
       // Compare the provided password with the hashed password from the database
-      const passwordMatch = await bcrypt.compare(password, hashedPassword);
+      const passwordMatch = await bcrypt.compare(password, password_hash);
 
-      const token = jwt.sign({ userId: result.rows[0].user_id }, 'your_jwt_secret', { expiresIn: '1h' }); 
-      // OTP is verified successfully, send success response
       if (passwordMatch) {
-        console.log("USER LOGIN: USER LOGGEDIN SUCCESSFULLY.");
-        res.status(200).json({ success: true, message: "Login successful",userData: result.rows , token:token});
+        const token = jwt.sign({ userId: userData.user_id }, 'your_jwt_secret', { expiresIn: '1h' }); 
+        console.log("USER LOGIN: USER LOGGED IN SUCCESSFULLY.");
+        res.status(200).json({ success: true, message: "Login successful", userData, token });
       } else {
         console.log("USER LOGIN: USER PASSWORD INCORRECT.");
         res.status(200).json({ success: false, message: "Incorrect password" });
@@ -37,7 +35,7 @@ const userLogin = async (req, res) => {
     }
   } catch (error) {
     console.log("USER LOGIN: INTERNAL SERVER ERROR.");
-    console.error("ERROR LOGIN IN:", error);
+    console.error("ERROR LOGGING IN:", error);
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
--- server/services/createUser.js
+++ server/services/createUser.js
@@ -5,11 +5,11 @@ async function createUser(firstName, lastName, email, phoneNumber, passwordHash)
     const db = connectToDatabase();
     // Insert user data into the database
     const insertQuery = `
-      INSERT INTO users (first_name, last_name, email, phone_number, password_hash)
-      VALUES ($1, $2, $3, $4, $5)
-      RETURNING user_id, first_name, last_name, email, phone_number
+      INSERT INTO users (first_name, last_name, email, phone_number, password_hash, role)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING user_id, first_name, last_name, email, phone_number, role
     `;
-    const insertValues = [firstName, lastName, email, phoneNumber, passwordHash];
+    const insertValues = [firstName, lastName, email, phoneNumber, passwordHash, 'user'];
     const { rows } = await db.query(insertQuery, insertValues);
 
     const userData = rows[0]; // Get the user data from the inserted row
